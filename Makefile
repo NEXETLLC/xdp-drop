552155a2c30e78a49240ba7a8c99d15c71b95160
evsio0n <bbq2001820@gmail.com>
2023-05-07 03:46:22 +0800

    First Commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+#clang -O2 -target bpf -c xdp_prog.c -o  xdp_prog.o
+
+default: xdp_prog
+
+xdp_prog:
+	clang -O2 -target bpf -c xdp_prog.c -o  xdp_prog.o
---
+++ xdp_prog.c
@@ -0,0 +1,59 @@
+#include <stdint.h>
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/in.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#define SEC(NAME) __attribute__((section(NAME), used))
+
+#define htons(x) ((__be16)___constant_swab16((x)))
+#define htonl(x) ((__be32)___constant_swab32((x)))
+
+struct vlan_hdr {
+	__be16 h_vlan_TCI;
+	__be16 h_vlan_encapsulated_proto;
+};
+
+// #define bpf_printk(fmt, ...)                              \
+// ({                                                        \
+//   char ____fmt[] = fmt;                                   \
+//   bpf_trace_printk(____fmt, sizeof(____fmt),              \
+//                    ##__VA_ARGS__);                        \
+// })
+
+SEC("xdp_drop")
+int myxdpprogram(struct xdp_md *ctx) {
+
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct ethhdr *eth = data;
+
+	uint64_t nh_off = sizeof(*eth);
+    if (data + nh_off > data_end) {
+		return XDP_PASS;
+	}
+
+	uint16_t h_proto = eth->h_proto;
+	int i;
+    //found vlan 599 600 601 and udp src port 123 then drop
+    if (h_proto == htons(ETH_P_8021Q)) {
+        struct vlan_hdr *vhdr;
+        vhdr = data + nh_off;
+		nh_off += sizeof(struct vlan_hdr);
+		h_proto = vhdr->h_vlan_encapsulated_proto;
+         //found vlan 599 600 601 and udp src port 123 then drop
+            if (vhdr->h_vlan_TCI == htons(599) || vhdr->h_vlan_TCI == htons(600) || vhdr->h_vlan_TCI == htons(601)) {
+                struct iphdr *iph = data + nh_off;
+                struct udphdr *udph = data + nh_off + sizeof(struct iphdr);
+                if (iph + 1 > data_end)
+                    return XDP_DROP;
+                if (iph->protocol == IPPROTO_UDP || udph->source == htons(123)) {
+                    return XDP_DROP;
+                }
+            }
+		}
+        return XDP_PASS;
+}
+
